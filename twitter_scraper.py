08c31632098f7e872f42c5fc83056ae43720341b
Kenneth Reitz <me@kennethreitz.org>
2018-02-22 10:24:37 -0500

    Update twitter_scraper.py

--- twitter_scraper.py
+++ twitter_scraper.py
@@ -5,7 +5,7 @@ def get_tweets(user, pages=25):
     url = f'https://twitter.com/i/profiles/show/{user}/timeline/tweets?include_available_features=1&include_entities=1&include_new_items_bar=true'
     headers = {
         'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'Referer': 'https://twitter.com/kennethreitz',
+        'Referer': f'https://twitter.com/{user}',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8',
         'X-Twitter-Active-User': 'yes',
         'X-Requested-With': 'XMLHttpRequest'
