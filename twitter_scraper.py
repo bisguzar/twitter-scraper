f6a87b42b33596481131d426632bb2b13bbba4ac
Recursing <buonanno.lorenzo@gmail.com>
2018-03-22 15:13:39 +0100

    Remove hidden elements from tweet text
    Probably there's a better way to ignore them

--- twitter_scraper.py
+++ twitter_scraper.py
@@ -35,7 +35,10 @@ def get_tweets(user, pages=25):
                 tweet_text_elements = tweet.find('.tweet-text')
                 if not tweet_text_elements:
                     continue
-                text = tweet_text_elements[0].full_text
+                text_container = tweet_text_elements[0]
+                for hidden_child in text_container.lxml.find_class('u-hidden'):
+                    hidden_child.drop_tree()
+                text = text_container.full_text.strip()
                 tweetId = tweet.find(
                     '.js-permalink')[0].attrs['data-conversation-id']
                 time = datetime.fromtimestamp(
