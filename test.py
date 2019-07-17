902793c05a930f6d4cd55c8514928401b12eaccb
m.hama <m.hama@mibel.cs.tsukuba.ac.jp>
2019-07-18 08:22:11 +0900

    Fix test.py

--- test.py
+++ test.py
@@ -6,19 +6,19 @@ class TestFamilyUnderscore(unittest.TestCase):
 
     def test_father(self):
         user = '_'
-        tweets = list(get_tweets(user=user, pages=1))
+        tweets = list(get_tweets(query=user, pages=1))
 
         self.assertTrue(tweets[0]['text'].__contains__('Want to feel old?'))
 
     def test_mother(self):
         user = '__'
-        tweets = list(get_tweets(user=user, pages=1))
+        tweets = list(get_tweets(query=user, pages=1))
 
         self.assertTrue(tweets[0]['text'].__contains__('It is a gift to be alive in the time of Beyoncé'))
 
     def test_child(self):
         user = '___'
-        tweets = list(get_tweets(user=user, pages=1))
+        tweets = list(get_tweets(query=user, pages=1))
 
         self.assertEqual(tweets[1]['text'], 'If I could, I would, but if I can’t, I wan’t.')
 
@@ -28,12 +28,12 @@ class TestPages(unittest.TestCase):
     def test_25pages(self):
         """I don't know why but in some cases it only crawls 2~5 pages"""
         user = 'kennethreitz'
-        tweets = list(get_tweets(user=user, pages=25))
+        tweets = list(get_tweets(query=user, pages=25))
         self.assertGreater(len(tweets), 486)
 
     def test_languages(self):
         user = 'fcbarcelona_jp'
-        tweets = list(get_tweets(user=user, pages=1))
+        tweets = list(get_tweets(query=user, pages=1))
         self.assertIn('likes', tweets[0])
         self.assertIsInstance(tweets[0]['replies'], int)
         self.assertGreaterEqual(tweets[1]['retweets'], 0)
