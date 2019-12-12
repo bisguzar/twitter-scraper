5452569fbb65e2a1c1a765602658b7d708c3ebbd
Buğra İşgüzar <biisguzar@gmail.com>
2019-12-12 16:59:16 +0300

    refactor profile class with requests-html

--- twitter_scraper/modules/profile.py
+++ twitter_scraper/modules/profile.py
@@ -7,6 +7,7 @@ session = HTMLSession()
 browser = mechanicalsoup.StatefulBrowser()
 browser.addheaders = [('User-agent', 'Firefox')]
 
+
 class Profile:
     """
         Parse twitter profile and split informations into class as attribute.
@@ -24,77 +25,79 @@ class Profile:
             - following_count
     """
     def __init__(self, username):
-        browser.open("https://twitter.com/"+username)
-        page = browser.get_current_page()
+        headers = {
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Referer': f'https://twitter.com/{username}',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8',
+            'X-Twitter-Active-User': 'yes',
+            'X-Requested-With': 'XMLHttpRequest',
+            'Accept-Language': 'en-US'
+        }
+
+        page = session.get(f"https://twitter.com/{username}", headers=headers)
         self.username = username
         self.__parse_profile(page)
 
     def __parse_profile(self, page):
-        # parse location, also check is username valid 
         try:
-            self.location = page.find(attrs={"class":"ProfileHeaderCard-locationText u-dir"}).contents[1].contents[0].strip()
-        except AttributeError:
+            html = HTML(html=page.text,
+                        url='bunk', default_encoding='utf-8')
+        except KeyError:
             raise ValueError(
-                    f'Oops! Either "@{self.username}" does not exist or is private.')
+                f'Oops! Either "{query}" does not exist or is private.')
+        except ParserError:
+            pass
 
-        # parse birthday
-        try:
-            self.birthday = page.find(attrs={"class":"ProfileHeaderCard-birthdateText u-dir"}).find().contents[0].strip().replace("Born ", "")
-        except:
+        # TODO: Check what kind of exception raising if no location
+        self.location = html.find('.ProfileHeaderCard-locationText')[0].text
+
+        # TODO: Check what kind of exception raising if no location
+        self.birthday = html.find('.ProfileHeaderCard-birthdateText')[0].text
+        if self.birthday:
+            self.birthday = self.birthday.replace('Born ', '')
+        else:
             self.birthday = None
 
-        # parse URL of profile photo
-        self.profile_photo = page.find(attrs={"class":"ProfileAvatar-image"}).attrs['src']
+        self.profile_photo = html.find('.ProfileAvatar-image')[0].attrs['src']
 
-        # parse full name
-        name_text = page.find("title").contents[0]
-        self.name = name_text[:name_text.find('(')].strip()
+        page_title = html.find('title')[0].text
+        self.name = page_title[:page_title.find('(')].strip()
 
-        # parse biography
-        self.biography = self.__process_paragraph(page.find(attrs={"class":"ProfileHeaderCard-bio u-dir"}).contents)
+        self.biography = html.find('.ProfileHeaderCard-bio')[0].text
 
-        # parse user's website adress
-        try:
-            self.website = page.find(attrs={'class': 'ProfileHeaderCard-urlText u-dir'}).find().contents[0].strip()
-        except:
-            self.website = None
+        self.website = html.find('.ProfileHeaderCard-urlText')[0].text
         
-        # parse count of followers
+
+        # scrape profile stats 
+        _stats_source = html.find("span[class=ProfileNav-value]")
+
+        # get total tweets count if available
         try:
-            q=page.find(attrs={"data-nav":"followers"})
-            self.followers_count = int(q.attrs["title"].split(' ')[0].replace(',',''))
+            q = _stats_source[0].attrs['data-count']
+            self.tweets_count = int(q)
         except:
-            self.followers_count = 0
-
-        # parse count of likes
-        q=page.find(attrs={"data-nav":"favorites"})
-        self.likes_count = int(q.attrs["title"].split(' ')[0].replace('.', ''))
-
-        # parse count of following
-        q=page.find(attrs={"data-nav":"following"})
-        self.following_count = int(q.attrs["title"].split(' ')[0].replace(',',''))
-
-        # parse count of tweets
-        q=page.find(attrs={"data-nav":"tweets"})
-        self.tweets_count = int(q.attrs["title"].split(' ')[0].replace(',',''))
-
-    def __process_paragraph(self, contents):
-        output = ''
-        links = []
-        for i in contents:
-            try:
-                output+=i
-            except:
-                if i.name=="a":
-                    tmp_txt, tmp_lnk = process_paragraph(i.contents)
-                    links+=tmp_lnk
-                    output+=tmp_txt#+'@['+i.attrs['href']+']'
-                    links.append(i.attrs['href'])
-                elif i.name in ['s', 'b']:
-                    tmp_txt, tmp_lnk = process_paragraph(i.contents)
-                    links+=tmp_lnk
-                    output+=tmp_txt
-        return output, links
+            self.tweets_count = None
+        
+        # get total following count if available
+        try:
+            q = _stats_source[1].attrs['data-count']
+            self.following_count = int(q)
+        except:
+            self.following_count = None
+        
+        # get total follower count if available
+        try:
+            q = _stats_source[2].attrs['data-count']
+            self.followers_count = int(q)
+        except:
+            self.followers_count = None
+        
+        # get total like count if available
+        try:
+            q = _stats_source[3].attrs['data-count']
+            self.likes_count = int(q)
+        except:
+            self.likes_count = None
 
     def __dir__(self):
         return [
@@ -110,5 +113,16 @@ class Profile:
             'following_count'
         ]
 
-    def __repr__(self):
-        return f'<profile {self.username}@twitter>'
+    def to_dict(self):
+        return dict(
+            name = self.name,
+            username = self.username,
+            birthday = self.birthday,
+            biography = self.biography,
+            website = self.website,
+            profile_photo = self.profile_photo,
+            likes_count = self.likes_count,
+            tweets_count = self.tweets_count,
+            followers_count = self.followers_count,
+            following_count = self.following_count
+        )
