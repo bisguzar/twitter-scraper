178ae56bebb247675553e338f29b95c439ed0db0
Buğra İşgüzar <Bura gzar>
2020-01-11 21:50:42 +0300

    replace htm to html

--- twitter_scraper/modules/trends.py
+++ twitter_scraper/modules/trends.py
@@ -7,11 +7,11 @@ def get_trends():
     trends = []
 
     html = session.get("https://twitter.com/i/trends").json()["module_html"]
-    htm = HTML(
+    html = HTML(
         html=html, url="bunk", default_encoding="utf-8"
     )
 
-    for trend_item in htm.find('li'):
+    for trend_item in html.find('li'):
         trend_text = trend_item.attrs['data-trend-name']
 
         trends.append(trend_text)
