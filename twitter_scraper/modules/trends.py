31babeb3bd0a80db5ddde2329d8506f61d8a8673
Bugra Isguzar <biisguzar@gmail.com>
2020-02-23 13:46:43 +0300

    add headers for trends request

--- twitter_scraper/modules/trends.py
+++ twitter_scraper/modules/trends.py
@@ -6,19 +6,22 @@ session = HTMLSession()
 def get_trends():
     trends = []
 
-    html = session.get("https://twitter.com/i/trends").json()["module_html"]
-    html = HTML(
-        html=html, url="bunk", default_encoding="utf-8"
-    )
+    headers = {
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8",
+        "X-Twitter-Active-User": "yes",
+        "X-Requested-With": "XMLHttpRequest",
+        "Accept-Language": "en-US",
+    }
 
-    for trend_item in html.find('li'):
-        trend_text = trend_item.attrs['data-trend-name']
+    html = session.get("https://twitter.com/i/trends", headers=headers)
+    html = html.json()["module_html"]
 
-        trends.append(trend_text)
-    
+    html = HTML(html=html, url="bunk", default_encoding="utf-8")
+
+    for trend_item in html.find("li"):
+        trend_text = trend_item.attrs["data-trend-name"]
+
+        trends.append(trend_text
+        
     return trends
-
-
-if __name__ == "__main__":
-    print(get_trends())
-
