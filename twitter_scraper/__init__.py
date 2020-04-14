515331cb299ae43fe94aa27e86ac9b50b799854c
Bugra Isguzar <biisguzar@gmail.com>
2020-04-14 23:30:11 +0300

    format codestyle via black

--- twitter_scraper/__init__.py
+++ twitter_scraper/__init__.py
@@ -2,4 +2,4 @@ from .modules.profile import Profile
 from .modules.tweets import get_tweets
 from .modules.trends import get_trends
 
-__all__ = ['Profile', 'get_tweets', 'get_trends']
+__all__ = ["Profile", "get_tweets", "get_trends"]
