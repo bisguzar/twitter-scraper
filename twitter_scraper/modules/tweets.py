437db7e7331172d4d2a06edfef36d1511ab9bdc8
Bugra Isguzar <biisguzar@gmail.com>
2020-07-17 00:01:16 +0300

    refactored and updated new search feature

--- twitter_scraper/modules/tweets.py
+++ twitter_scraper/modules/tweets.py
@@ -6,201 +6,173 @@ from lxml.etree import ParserError
 
 session = HTMLSession()
 
-def get_tweets(query=None, search_term=None, user_name=None, pages=25):
+def get_tweets(query=None, search=None, pages=25):
     """Gets tweets for a given user, via the Twitter frontend API."""
-    incompatibleSearch=False
-    scenario=0
-    #first check the logic: both search_term and user_name cannot be set:
-    if search_term and user_name or search_term and query or  user_name and query:
-        incompatibleSearch=True
-        scenario=1
-    if not query and not search_term and not user_name:
-        incompatibleSearch=True
-        scenario=2
 
-    if not incompatibleSearch:
+    if not query and not search:
+        raise RuntimeError("Please specify a 'query' or a 'search' to check the tweets on.")
+    elif query and search:
+        raise RuntimeError("Please specify only one of either a 'search' or 'query'.")	
 
-        after_part = (
-            f"include_available_features=1&include_entities=1&include_new_items_bar=true"
-        )
-        #here we will keep the existing query bits to ensure any old api calls
-        #dependent on the software will continue to function...
-        if query:
-            if query.startswith("#"):
-                query = quote(query)
-                url = f"https://twitter.com/i/search/timeline?f=tweets&vertical=default&q={query}&src=tyah&reset_error_state=false&"
-            else:
-                url = f"https://twitter.com/i/profiles/show/{query}/timeline/tweets?"
-        #now for the updates to enable specifically user_name searching...
-        elif user_name:
-            url = f"https://twitter.com/i/profiles/show/{user_name}/timeline/tweets?"
-        #and general search term compatibility
-        elif search_term:
-            search_term=quote(search_term)
-            url = f"https://twitter.com/i/search/timeline?f=tweets&vertical=default&q={search_term}&src=tyah&reset_error_state=false&"
+    after_part = (
+        f"include_available_features=1&include_entities=1&include_new_items_bar=true"
+    )
+    if not query: # if query not exists, it's a search method
+        search_term=quote(search)
+        url = f"https://twitter.com/i/search/timeline?f=tweets&vertical=default&q={search_term}&src=tyah&reset_error_state=false&"
 
-        url += after_part
-
-        headers = {
-            "Accept": "application/json, text/javascript, */*; q=0.01",
-            "Referer": f"https://twitter.com/{query}",
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8",
-            "X-Twitter-Active-User": "yes",
-            "X-Requested-With": "XMLHttpRequest",
-            "Accept-Language": "en-US",
-        }
-        #Here we will integrate the new general search functionality
-
-
-
-        def gen_tweets(pages):
-            r = session.get(url, headers=headers)
-
-            while pages > 0:
-                try:
-                    html = HTML(
-                        html=r.json()["items_html"], url="bunk", default_encoding="utf-8"
-                    )
-                except KeyError:
-                    raise ValueError(
-                        f'Oops! Either "{query}" does not exist or is private.'
-                    )
-                except ParserError:
-                    break
-
-                comma = ","
-                dot = "."
-                tweets = []
-                for tweet, profile in zip(
-                    html.find(".stream-item"), html.find(".js-profile-popup-actionable")
-                ):
-                    # 10~11 html elements have `.stream-item` class and also their `data-item-type` is `tweet`
-                    # but their content doesn't look like a tweet's content
-                    try:
-                        text = tweet.find(".tweet-text")[0].full_text
-                    except IndexError:  # issue #50
-                        continue
-
-
-                    tweet_id = tweet.attrs["data-item-id"]
-
-                    tweet_url = profile.attrs["data-permalink-path"]
-
-                    username = profile.attrs["data-screen-name"]
-
-                    user_id = profile.attrs["data-user-id"]
-
-                    is_pinned = bool(tweet.find("div.pinned"))
-
-                    time = datetime.fromtimestamp(
-                        int(tweet.find("._timestamp")[0].attrs["data-time-ms"]) / 1000.0
-                    )
-
-                    interactions = [x.text for x in tweet.find(".ProfileTweet-actionCount")]
-
-                    replies = int(
-                        interactions[0].split(" ")[0].replace(comma, "").replace(dot, "")
-                        or interactions[3]
-                    )
-
-                    retweets = int(
-                        interactions[1].split(" ")[0].replace(comma, "").replace(dot, "")
-                        or interactions[4]
-                        or interactions[5]
-                    )
-
-                    likes = int(
-                        interactions[2].split(" ")[0].replace(comma, "").replace(dot, "")
-                        or interactions[6]
-                        or interactions[7]
-                    )
-
-                    hashtags = [
-                        hashtag_node.full_text
-                        for hashtag_node in tweet.find(".twitter-hashtag")
-                    ]
-
-                    urls = [
-                        url_node.attrs["data-expanded-url"]
-                        for url_node in (
-                            tweet.find("a.twitter-timeline-link:not(.u-hidden)") +
-                            tweet.find("[class='js-tweet-text-container'] a[data-expanded-url]")
-                        )
-                    ]
-                    urls = list(set(urls)) # delete duplicated elements
-
-                    photos = [
-                        photo_node.attrs["data-image-url"]
-                        for photo_node in tweet.find(".AdaptiveMedia-photoContainer")
-                    ]
-
-                    is_retweet = (
-                        True
-                        if tweet.find(".js-stream-tweet")[0].attrs.get(
-                            "data-retweet-id", None
-                        )
-                        else False
-                    )
-
-                    videos = []
-                    video_nodes = tweet.find(".PlayableMedia-player")
-                    for node in video_nodes:
-                        styles = node.attrs["style"].split()
-                        for style in styles:
-                            if style.startswith("background"):
-                                tmp = style.split("/")[-1]
-                                video_id = (
-                                    tmp[: tmp.index(".jpg")]
-                                    if ".jpg" in tmp
-                                    else tmp[: tmp.index(".png")]
-                                    if ".png" in tmp
-                                    else None
-                                )
-                                videos.append({"id": video_id})
-
-                    tweets.append(
-                        {
-                            "tweetId": tweet_id,
-                            "tweetUrl": tweet_url,
-                            "username": username,
-                            "userId": user_id,
-                            "isRetweet": is_retweet,
-                            "isPinned": is_pinned,
-                            "time": time,
-                            "text": text,
-                            "replies": replies,
-                            "retweets": retweets,
-                            "likes": likes,
-                            "entries": {
-                                "hashtags": hashtags,
-                                "urls": urls,
-                                "photos": photos,
-                                "videos": videos,
-                            },
-                        }
-                    )
-
-                last_tweet = html.find(".stream-item")[-1].attrs["data-item-id"]
-
-                for tweet in tweets:
-                    tweet["text"] = re.sub(r"(\S)http", "\g<1> http", tweet["text"], 1)
-                    tweet["text"] = re.sub(
-                        r"(\S)pic\.twitter", "\g<1> pic.twitter", tweet["text"], 1
-                    )
-                    yield tweet
-
-                r = session.get(url, params={"max_position": last_tweet}, headers=headers)
-                pages += -1
-
-        yield from gen_tweets(pages)
-
+    elif query.startswith("#"):
+        query = quote(query)
+        url = f"https://twitter.com/i/search/timeline?f=tweets&vertical=default&q={query}&src=tyah&reset_error_state=false&"
     else:
-        tupletoyield=None
-        if scenario==1:
-            tupletoyield=("Please specify only one of either a search_term, a query, or a user_name",)
-        elif scenario == 2:
-            tupletoyield=("Please specify a query, a search_term, or a user_name to check the tweets on.",)
-        yield tupletoyield
+        url = f"https://twitter.com/i/profiles/show/{query}/timeline/tweets?"
+    url += after_part
+
+    headers = {
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Referer": f"https://twitter.com/{query}",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8",
+        "X-Twitter-Active-User": "yes",
+        "X-Requested-With": "XMLHttpRequest",
+        "Accept-Language": "en-US",
+    }
+
+    def gen_tweets(pages):
+        r = session.get(url, headers=headers)
+
+        while pages > 0:
+            try:
+                html = HTML(
+                    html=r.json()["items_html"], url="bunk", default_encoding="utf-8"
+                )
+            except KeyError:
+                raise ValueError(
+                    f'Oops! Either "{query}" does not exist or is private.'
+                )
+            except ParserError:
+                break
+
+            comma = ","
+            dot = "."
+            tweets = []
+            for tweet, profile in zip(
+                html.find(".stream-item"), html.find(".js-profile-popup-actionable")
+            ):
+                # 10~11 html elements have `.stream-item` class and also their `data-item-type` is `tweet`
+                # but their content doesn't look like a tweet's content
+                try:
+                    text = tweet.find(".tweet-text")[0].full_text
+                except IndexError:  # issue #50
+                    continue
+
+
+                tweet_id = tweet.attrs["data-item-id"]
+                tweet_url = profile.attrs["data-permalink-path"]
+                username = profile.attrs["data-screen-name"]
+                user_id = profile.attrs["data-user-id"]
+                is_pinned = bool(tweet.find("div.pinned"))
+
+                time = datetime.fromtimestamp(
+                    int(tweet.find("._timestamp")[0].attrs["data-time-ms"]) / 1000.0
+                )
+
+                interactions = [x.text for x in tweet.find(".ProfileTweet-actionCount")]
+
+                replies = int(
+                    interactions[0].split(" ")[0].replace(comma, "").replace(dot, "")
+                    or interactions[3]
+                )
+
+                retweets = int(
+                    interactions[1].split(" ")[0].replace(comma, "").replace(dot, "")
+                    or interactions[4]
+                    or interactions[5]
+                )
+
+                likes = int(
+                    interactions[2].split(" ")[0].replace(comma, "").replace(dot, "")
+                    or interactions[6]
+                    or interactions[7]
+                )
+
+                hashtags = [
+                    hashtag_node.full_text
+                    for hashtag_node in tweet.find(".twitter-hashtag")
+                ]
+
+                urls = [
+                    url_node.attrs["data-expanded-url"]
+                    for url_node in (
+                        tweet.find("a.twitter-timeline-link:not(.u-hidden)") +
+                        tweet.find("[class='js-tweet-text-container'] a[data-expanded-url]")
+                    )
+                ]
+                urls = list(set(urls)) # delete duplicated elements
+
+                photos = [
+                    photo_node.attrs["data-image-url"]
+                    for photo_node in tweet.find(".AdaptiveMedia-photoContainer")
+                ]
+
+                is_retweet = (
+                    True
+                    if tweet.find(".js-stream-tweet")[0].attrs.get(
+                        "data-retweet-id", None
+                    )
+                    else False
+                )
+
+                videos = []
+                video_nodes = tweet.find(".PlayableMedia-player")
+                for node in video_nodes:
+                    styles = node.attrs["style"].split()
+                    for style in styles:
+                        if style.startswith("background"):
+                            tmp = style.split("/")[-1]
+                            video_id = (
+                                tmp[: tmp.index(".jpg")]
+                                if ".jpg" in tmp
+                                else tmp[: tmp.index(".png")]
+                                if ".png" in tmp
+                                else None
+                            )
+                            videos.append({"id": video_id})
+
+                tweets.append(
+                    {
+                        "tweetId": tweet_id,
+                        "tweetUrl": tweet_url,
+                        "username": username,
+                        "userId": user_id,
+                        "isRetweet": is_retweet,
+                        "isPinned": is_pinned,
+                        "time": time,
+                        "text": text,
+                        "replies": replies,
+                        "retweets": retweets,
+                        "likes": likes,
+                        "entries": {
+                            "hashtags": hashtags,
+                            "urls": urls,
+                            "photos": photos,
+                            "videos": videos,
+                        },
+                    }
+                )
+
+            last_tweet = html.find(".stream-item")[-1].attrs["data-item-id"]
+
+            for tweet in tweets:
+                tweet["text"] = re.sub(r"(\S)http", "\g<1> http", tweet["text"], 1)
+                tweet["text"] = re.sub(
+                    r"(\S)pic\.twitter", "\g<1> pic.twitter", tweet["text"], 1
+                )
+                yield tweet
+
+            r = session.get(url, params={"max_position": last_tweet}, headers=headers)
+            pages += -1
+
+    yield from gen_tweets(pages)
 
 
 # for searching:
